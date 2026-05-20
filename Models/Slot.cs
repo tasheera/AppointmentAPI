9770c19557a48851b6efa06efa3355aaf6e3dbe1
tasheera <tasheeraabhishek@gmail.com>
2026-05-20 14:57:45 +0530

    add ProviderId and Provider reference to Slot model

--- Models/Slot.cs
+++ Models/Slot.cs
@@ -1,12 +1,12 @@
-
 namespace AppointmentAPI.Models
-
 {
     public class Slot
     {
         public int Id { get; set; }
         public int ServiceId { get; set; }
         public Service Service { get; set; } = null!;
+        public int ProviderId { get; set; }
+        public Provider Provider { get; set; } = null!;
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public bool IsBooked { get; set; } = false;
